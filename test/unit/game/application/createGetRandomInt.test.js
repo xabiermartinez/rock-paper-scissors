e8c15fdc243bbac539ba488f50bc0b3564cf846d
Xabi Martínez <xabi.ma@gmail.com>
2022-03-01 15:46:07 +0100

    Add getRandomInt logic

---
+++ test/unit/game/application/createGetRandomInt.test.js
@@ -0,0 +1,14 @@
+const createGetRandomInt = require("../../../../src/game/application/createGetRandomInt");
+const getRandomInt = createGetRandomInt();
+
+describe("randomInt", function () {
+    it("should return an number", function () {
+        const randomInt = getRandomInt(2);
+        expect(typeof randomInt).toBe("number");
+    });
+
+    it("should return a number from 0 to specified number", function () {
+        const randomInt = getRandomInt(2);
+        expect([0, 1, 2]).toContain(randomInt);
+    });
+});
