2bc29fcc36e48f149de8eacabf7d21a500bce438
Xabi Martínez <xabi.ma@gmail.com>
2022-03-03 11:15:52 +0100

    Refactor botMove

--- test/unit/game/application/createBotMove.test.js
+++ test/unit/game/application/createBotMove.test.js
@@ -3,6 +3,6 @@ const { botMove } = require("../../../../src/game/application");
 describe("botMove", function () {
     it("should return rock, paper or scissors", function () {
         const move = botMove();
-        expect(["rock", "paper", "scissors"]).toContain(move);
+        expect(["rock", "paper", "scissors"]).toContain(move.move);
     });
 });
