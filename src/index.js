a483955160f370ca5dd12acfb37b0a3778881fe1
Xabi Martínez <xabi.ma@gmail.com>
2022-03-03 11:25:45 +0100

    Add createPlayGame controller with validation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 
+const { playGameValidation } = require("./game/infrastructure/validation");
+const { playGame } = require("./game/interfaces");
+const createRequest = require("./game/infrastructure/createRequest");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.post("/api/play", createRequest(playGame, playGameValidation));
 
 module.exports = app.listen(3000, () => {
     console.log("Server started (http://localhost:3000/ externally) !");
